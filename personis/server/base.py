8745d585faa34f1abf94ff4674bb738fc3a2f5ed
James Uther <james.uther@gmail.com>
2012-08-15 16:24:34 +1000

    fdsfs

--- personis/server/base.py
+++ personis/server/base.py
@@ -426,8 +426,8 @@ class Access(resolvers.Access,ev_filters.Access):
 			subs = None
 		try:
 			contexts = os.listdir(self.curcontext)
-			print 'c1',contexts
-			contexts = [x for x in contexts if os.path.isdir(self.curcontext+"/"+x)]
+			print 'c1',self.curcontext, contexts
+			contexts = [x for x in contexts if os.path.isdir(os.path.join(self.curcontext,x)]
 			print 'c2',contexts
 		except OSError as e:
 			raise ValueError("Component/Context not found: %s/%s" % (context, view))
